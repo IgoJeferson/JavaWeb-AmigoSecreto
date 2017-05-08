845fdca593fe16e179b8e273ba03b8cf6ca9520f
Igo Jeferson Barros Coelho <igor.jeferson17@gmail.com>
2017-05-07 21:00:00 -0300

    m

---
+++ src/br/com/fiap/amigosecreto/entity/Usuario.java
@@ -0,0 +1,8 @@
+package br.com.fiap.amigosecreto.entity;
+
+public class Usuario {
+
+	private Long cpf;
+	private String password;
+	
+}
---
+++ src/br/com/fiap/amigosecreto/enums/Perfil.java
@@ -0,0 +1,8 @@
+package br.com.fiap.amigosecreto.enums;
+
+public enum Perfil {
+
+	ADMINISTRADOR,
+	PARTICIPANTE
+	
+}
