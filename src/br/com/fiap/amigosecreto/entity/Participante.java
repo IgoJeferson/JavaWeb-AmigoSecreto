f6b7b93ed2387b6307296b61d6bfeb04f367e494
Igo Jeferson Barros Coelho <igor.jeferson17@gmail.com>
2017-05-07 21:18:35 -0300

    Opa

--- src/br/com/fiap/amigosecreto/entity/Participante.java
+++ src/br/com/fiap/amigosecreto/entity/Participante.java
@@ -1,5 +1,28 @@
 package br.com.fiap.amigosecreto.entity;
 
 public class Participante {
+	
+	private Integer id;
+	private Usuario usuario;
+	private Participante amigoSecreto;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public Participante getAmigoSecreto() {
+		return amigoSecreto;
+	}
+	public void setAmigoSecreto(Participante amigoSecreto) {
+		this.amigoSecreto = amigoSecreto;
+	}
 
 }
--- src/br/com/fiap/amigosecreto/entity/Usuario.java
+++ src/br/com/fiap/amigosecreto/entity/Usuario.java
@@ -1,8 +1,43 @@
 package br.com.fiap.amigosecreto.entity;
 
+import br.com.fiap.amigosecreto.enums.Perfil;
+
 public class Usuario {
 
+	private Integer id;
 	private Long cpf;
+	private String nome;
 	private String password;
+	private Perfil perfil;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Long getCpf() {
+		return cpf;
+	}
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Perfil getPerfil() {
+		return perfil;
+	}
+	public void setPerfil(Perfil perfil) {
+		this.perfil = perfil;
+	}
 	
 }
